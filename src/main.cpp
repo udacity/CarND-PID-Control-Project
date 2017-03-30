f61173251632140f07cfd8294eac3c648a8a0ead
Dominique Luna <dluna132@gmail.com>
2017-03-30 14:31:06 -0700

    Update main.cpp
    there's a pi in math.h apparently

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 using json = nlohmann::json;
 
 // For converting back and forth between radians and degrees.
-constexpr double pi() { return std::atan(1)*4; }
+constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
 
