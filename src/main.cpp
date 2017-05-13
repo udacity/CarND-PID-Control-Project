2aab9a649cf80ce9f3fe3a3b22b6712046552eda
swirlingsand <anthonysarkis@gmail.com>
2017-05-13 12:51:47 -0600

    works with sim! (Ralph-68's fix!)
    see
    https://discussions.udacity.com/t/project-build-for-windows-users/245846/20

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #define _USE_MATH_DEFINES
 
-#include <uWS/uWS.h>
+#include "uWS/uWS.h"
 #include <iostream>
 #include "json.hpp"
 #include "PID.h"
@@ -40,13 +40,14 @@ std::stringstream hasData(std::string s) {
 
 int main()
 {
+
   uWS::Hub h;
 
   PID pid;
   // TODO: Initialize the pid variable.
   
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> *ws, char *data, size_t length, uWS::OpCode opCode) {
+  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -61,7 +62,6 @@ int main()
           double cte = std::stod(j[1]["cte"].get<std::string>());
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-          double steer_value;
           /*
           * TODO: Calcuate steering value here, remember the steering value is
           * [-1, 1].
@@ -78,23 +78,23 @@ int main()
           msgJson["throttle"] = 0.3;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
-          (*ws).send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+          (ws).send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
       }
       else {
         // Manual driving
         std::string msg = "42[\"manual\",{}]";
-        (*ws).send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+        (ws).send(msg.data(), msg.length(), uWS::OpCode::TEXT);
       }
     }
   });
 
-  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> *ws, uWS::HttpRequest req) {
+  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
     std::cout << "Connected!!!" << std::endl;
   });
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> *ws, int code, char *message, size_t length) {
-    (*ws).close();
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
+    (ws).close();
     std::cout << "Disconnected" << std::endl;
   });
 
