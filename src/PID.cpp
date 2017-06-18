749e990ac8491103901f339f7297b26577711b35
Mihir Rajput <mihirrajput1123@gmail.com>
2017-06-18 13:27:33 -0400

    Initial check.
    Verify if the code structure is correct and I have a communication with the simulator.

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,22 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	PID::Kp = Kp;
+	PID::Kd = Kd;
+	PID::Ki = Ki;
+	p_error = 0.0;
+	d_error = 0.0;
+	i_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+	d_error = (cte - p_error);
+	p_error = cte;
+	i_error += cte;
 }
 
 double PID::TotalError() {
+	
+	return -PID::Kp * p_error - PID::Kd * d_error - PID::Ki * i_error;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  pid.Init(1, 0, 0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,16 +58,17 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
-          // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+		  pid.UpdateError(cte);
+		  steer_value = pid.TotalError();
+		  // DEBUG
+		  std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
-          json msgJson;
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
-          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+		  json msgJson;
+		  msgJson["steering_angle"] = steer_value;
+		  msgJson["throttle"] = 0.3;
+		  auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+		  std::cout << msg << std::endl;
+		  ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
       } else {
         // Manual driving
