0e8cf9c4f492633b8021e78abe78c19be3c5d8f2
ttakahas555 <ttakahashi07@gmail.com>
2017-09-16 02:50:07 -0400

    Implemented Initi function.

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,6 +11,9 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
@@ -18,4 +21,3 @@ void PID::UpdateError(double cte) {
 
 double PID::TotalError() {
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,8 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  pid.Init(0.2, 3.0, 0.004);
+
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
