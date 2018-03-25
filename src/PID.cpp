cf793e4782af059798f07a57c3c6adc500556117
Alexey Zasimov <zasimov@gmail.com>
2018-03-25 23:58:00 +0300

    implement PID

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,21 +1,66 @@
 #include "PID.h"
 
+#include <iostream>
+
 using namespace std;
 
-/*
-* TODO: Complete the PID class.
-*/
 
-PID::PID() {}
+PID::PID(double tau_p, double tau_i, double tau_d)
+  : p_cte_(0.0), i_cte_(0.0), d_cte_(0.0) {
 
-PID::~PID() {}
-
-void PID::Init(double Kp, double Ki, double Kd) {
+  tau_[kP] = tau_p;
+  tau_[kI] = tau_i;
+  tau_[kD] = tau_d;
 }
+
 
 void PID::UpdateError(double cte) {
+  d_cte_ = cte - p_cte_;
+  p_cte_ = cte;
+  i_cte_ =+ cte;
 }
+
 
 double PID::TotalError() {
+  std::cout << tau_[kP] << " " << tau_[kI] << " " << tau_[kD] << std::endl;
+  return (tau_[kP] * p_cte_ +
+	  tau_[kI] * i_cte_ +
+	  tau_[kD] * d_cte_);
 }
 
+
+void PID::Twiddle(const double tolerance) {
+  double dp[3] = {1.0, 1.0, 1.0};
+
+  double best_err = TotalError();
+
+  int it = 0;
+  while ((dp[0] + dp[1] + dp[2]) > tolerance) {
+    std::cout << "Iteration " << it << " best error " << best_err << std::endl;
+
+    for (unsigned i = 0; i < 3; i++) {
+      tau_[i] += dp[i];
+
+      double err = TotalError();
+
+      if (err < best_err) {
+	best_err = err;
+	dp[i] *= 1.1;
+      } else {
+	tau_[i] -= 2 * dp[i];
+
+	err = TotalError();
+
+	if (err < best_err) {
+	  best_err = err;
+	  dp[i] *= 1.05;
+	} else {
+	  tau_[i] += dp[i];
+	  dp[i] *= 0.95;
+	}
+      }
+    }
+
+    it++;
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -32,8 +32,7 @@ int main()
 {
   uWS::Hub h;
 
-  PID pid;
-  // TODO: Initialize the pid variable.
+  PID pid(0.2, 0.0003, 3.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -50,14 +49,17 @@ int main()
           double cte = std::stod(j[1]["cte"].get<std::string>());
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-          double steer_value;
-          /*
-          * TODO: Calcuate steering value here, remember the steering value is
-          * [-1, 1].
-          * NOTE: Feel free to play around with the throttle and speed. Maybe use
-          * another PID controller to control the speed!
-          */
-          
+
+	  pid.UpdateError(cte);
+
+	  double steer_value = -pid.TotalError();
+	  if (steer_value > 1.0) {
+	    steer_value = 1.0;
+	  }
+	  if (steer_value < -1.0) {
+	    steer_value = -1.0;
+	  }
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
